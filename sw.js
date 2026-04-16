3a7a470e68ae492e9c1653789b54d9c54d70f3d8
Wagurit <salaquarda@wagurit.de>
2026-04-16 16:54:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lagerchemie-v10';
+const CACHE = 'lagerchemie-v11';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
